cf395db1d756ac166cc4b04d028056e972e89912
Eric <ericdo.617@gmail.com>
2017-09-27 00:33:15 +0000

    Removed the test suite since it doesn't apply to this project. Added geolocation.

--- weather.js
+++ weather.js
@@ -1,1 +1,11 @@
 console.log("CONNECTED");
+
+if (navigator.geolocation) {
+  window.navigator.geolocation.getCurrentPosition(function(pos) {
+      var crd = pos.coords;
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude : ${crd.longitude}`);
+      console.log(`Altitude : ${crd.altitude}`);
+  });
+}
