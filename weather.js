4fed4fb608c73da3baa10d2f79759f8f89d75ea5
Eric Do <ericdo.617@gmail.com>
2017-09-27 00:08:55 -0700

    Added call to weather API

--- weather.js
+++ weather.js
@@ -1,11 +1,24 @@
 console.log("CONNECTED");
 
+var lat = 0;
+var lon = 0;
+
 if (navigator.geolocation) {
   window.navigator.geolocation.getCurrentPosition(function(pos) {
       var crd = pos.coords;
+      lat = Math.round(crd.latitude);
+      lon = Math.round(crd.longitude);
+
       console.log('Your current position is:');
-      console.log(`Latitude : ${crd.latitude}`);
-      console.log(`Longitude : ${crd.longitude}`);
-      console.log(`Altitude : ${crd.altitude}`);
+      console.log(`Latitude : ${lat}`);
+      console.log(`Longitude : ${lon}`);
   });
+
+  getWeather();
+}
+
+function getWeather() {
+    $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon, function(json) {
+        console.log(JSON.stringify(json));
+    });
 }
