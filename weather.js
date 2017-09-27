dc5dd6275c03243d535e9e5644228e9d9aee2faa
Eric Do <ericdo.617@gmail.com>
2017-09-27 12:40:07 -0700

    set up html

--- weather.js
+++ weather.js
@@ -1,7 +1,9 @@
-console.log("CONNECTED");
-
 var lat = 0;
 var lon = 0;
+var weather = "";
+var icon = "";
+var celsius = 0;
+var farenheit = 0;
 
 if (navigator.geolocation) {
   window.navigator.geolocation.getCurrentPosition(function(pos) {
@@ -20,14 +22,23 @@ if (navigator.geolocation) {
 function getWeather() {
     $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon, function(json) {
         console.log(JSON.stringify(json));
-        console.log(json.weather[0].main);
-        console.log(json.main.temp);
-        console.log((json.main.temp) * 1.8 + 32);
 
-        var weather = json.weather[0].main;
-        var celsius = Math.round(json.main.temp * 100) / 100;;
-        var farenheit = Math.round((celsius * 1.8 + 32) * 100 ) / 100;
+        weather = json.weather[0].main;
+        icon = json.weather[0].icon;
+        celsius = Math.round(json.main.temp * 100) / 100;;
+        farenheit = Math.round((celsius * 1.8 + 32) * 100 ) / 100;
         console.log("Farenheight: " + farenheit);
         console.log("Celsius: " + celsius);
+        console.log("Icon URL: " + icon);
     });
 }
+
+function printWeather () {
+    console.log(weather);
+    $("#temperature").html(farenheit);
+}
+
+$(document).ready(function(){
+   
+    setInterval(printWeather, 5000);
+});
