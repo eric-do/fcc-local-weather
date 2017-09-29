07df9ddb6261ae5fca48a7945e8ea1ace4717fe0
Eric Do <ericdo.617@gmail.com>
2017-09-28 23:48:57 -0700

    Added asynchronous implementation of geolocation and weather API

--- weather.js
+++ weather.js
@@ -1,44 +1,49 @@
-var lat = 0;
-var lon = 0;
-var weather = "";
-var icon = "";
-var celsius = 0;
-var farenheit = 0;
 
-if (navigator.geolocation) {
-  window.navigator.geolocation.getCurrentPosition(function(pos) {
-      var crd = pos.coords;
-      lat = Math.round(crd.latitude);
-      lon = Math.round(crd.longitude);
-
-      console.log('Your current position is:');
-      console.log(`Latitude : ${lat}`);
-      console.log(`Longitude : ${lon}`);
-  });
-
-  getWeather();
-}
-
-function getWeather() {
-    $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon, function(json) {
-        console.log(JSON.stringify(json));
-
-        weather = json.weather[0].main;
-        icon = json.weather[0].icon;
-        celsius = Math.round(json.main.temp * 100) / 100;;
-        farenheit = Math.round((celsius * 1.8 + 32) * 100 ) / 100;
-        console.log("Farenheight: " + farenheit);
-        console.log("Celsius: " + celsius);
-        console.log("Icon URL: " + icon);
-    });
-}
-
-function printWeather () {
-    console.log(weather);
-    $("#temperature").html(farenheit);
-}
 
 $(document).ready(function(){
-   
-    setInterval(printWeather, 5000);
+    showWeather();
+    
+    function showWeather() {
+        var lat = 0;
+        var lon = 0;
+        var weatherJSON = "";
+        var crd;
+    
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error);
+        }
+    
+        function success(pos) {
+            crd = pos.coords;
+            lat = Math.round(crd.latitude);
+            lon = Math.round(crd.longitude);
+
+            getWeather(lat, lon);
+        }
+
+        function error(err) {
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+            $("#condition").html("Your browser does not support Geolocation!");
+        }
+    }
+
+    function getWeather(lat, lon) {
+        return  $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon)
+        .then(function(json) { 
+            printWeather(json);
+         });
+     }
+     
+     function printWeather (json) {
+         var condition = json.weather[0].main;
+         var icon = json.weather[0].icon;
+         var celsius = Math.round(json.main.temp * 100) / 100;;
+         var farenheit = Math.round((celsius * 1.8 + 32) * 100 ) / 100;
+     
+         $("#condition").html(condition);
+         $("#temperature").html(farenheit);
+     }
 });
+
+
+
